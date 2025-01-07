1d77c768787737a2ca0f1159cdc3d080085f073c
zhaojo <st180794@stud.uni-stuttgart.de>
2025-01-07 13:18:57 +0100

    key

--- cognitive-assistant-server/scripts/constants.py
+++ cognitive-assistant-server/scripts/constants.py
@@ -6,8 +6,8 @@ ROOT = os.path.dirname(__file__)
 MAX_IMAGES = 5
 
 #OpenAI API Key
-API_KEY = "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" #Add your OpenAI API key here
+API_KEY = "" #Add your OpenAI API key here
 
-API_KEY_ME = "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" #Add your OpenAI API key here
+API_KEY_ME = "" #Add your OpenAI API key here
 
 IMAGE_FOLDER = "./static/images/"
